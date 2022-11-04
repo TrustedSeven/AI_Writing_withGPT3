3de2d372f9efb4dec88f8682fa91661d34480568
TrustedSeven <117359800+TrustedSeven@users.noreply.github.com>
2022-11-03 22:01:41 -0500

    homepage modified

--- README.md
+++ README.md
@@ -1,17 +1,40 @@
-## A Demo GPT-3 Powered Web App, created using NextJS & ReactJS
+# [Tailwind VPN Landingpage](https://next-landing-vpn.vercel.app/) - Free Landingpage Template Apps
 
-## QuickStart
-1. Clone this repo
-2. update the OpenAI key in .env.local file:
-    Sample .env.local file
-    
-    OPENAI_API_KEY=your-api-key
-    
-4. Install node modules
-5. start the dev server with "yarn dev" or "npm run dev" command
+Tailwind VPN Landingpage is an open source, apps landing page template for [Tailwind CSS](https://tailwindcss.com/) and[ NextJS](nextjs.org/) coded by [Faldi](twitter.com/f2aldi) and design from [Didi](https://twitter.com/didiikurniawann).
 
-For a Detailed How to Guide, on how to create this yourself, checkout this [blog post](https://harishgarg.com/writing/how-to-build-a-serverless-gpt-3-powered-using-nextjs-react/).
+![Landing Page](./landingpage.png)
 
-* [Follow me on Twitter](https://twitter.com/harishkgarg).
+## Getting Started
 
-* [Buy Me Coffee](https://www.buymeacoffee.com/harishgarg)
+Choose one of the following options to get started:
+
+- [Download the latest release](https://github.com/naufaldi/next-landing-vpn/archive/main.zip)
+- Clone the repo: `git clone https://github.com/naufaldi/next-landing-vpn.git`
+- Fork the repo
+
+## About the Template
+
+- Template building using NextJS Version 10
+- Tailwind v2.0
+
+## Feature Template
+
+- Using [NextJS Image](https://nextjs.org/docs/api-reference/next/image) for Image Optimization
+- Slider using [React Slick](https://react-slick.neostack.com/docs/api)
+- Smooth Scrolling and Active menu using [React Scroll](https://www.npmjs.com/package/react-scroll)
+
+## Bugs and Issues
+
+Have a bug or an issue with this template? [Open a new issue](https://github.com/naufaldi/next-landing-vpn/issues/new) here on GitHub.
+
+## Creator
+
+[Tailwind VPN Landingpage](https://next-landing-vpn.vercel.app/) was coded and modified by and is maintained by **[me](https://github.com/naufaldi/)**, and dekstop design by [Didi Kurniawan](https://twitter.com/didiikurniawann)
+
+## Copyright and License
+
+Code released under the MIT license.
+
+## To Do List Add Feature
+
+- [ ] Animation using Framer Motion
--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,18 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { useState, useEffect } from 'react';
+import Feature from "../components/Feature";
+import Pricing from "../components/Pricing";
+import Hero from "../components/Hero";
+import Layout from "../components/Layout/Layout";
+import SeoHead from "../components/SeoHead";
 
 export default function Home() {
-  const [data, setData] = useState( { text:'' });
-  const [query, setQuery] = useState();
-  const [search, setSearch] = useState();
-  const [isLoading, setIsLoading] = useState(false);
- 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (search) {
-      setIsLoading(true);
-      const res = await fetch(`/api/openai`, {
-        body: JSON.stringify({
-          name: search
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'POST'
-      })
-      const data = await res.json();
-      setData(data);
-      setIsLoading(false);
-    }};
- 
-    fetchData();
-  }, [search]);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>GPT-3 App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a>AI Article Generator</a>
-        </h1>
-
-        <p className={styles.description}>Built with NextJS & GPT-3 API</p>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3>Enter Artist:</h3>
-            <input
-          type="text"
-          value={query}
-          onChange={event => setQuery(event.target.value)}
-        />
-        <button
-          type="button"
-          onClick={() =>
-            setSearch(query)
-          }
-        >
-          Generate
-        </button>
-        
-          <h4>News Today</h4>  
-          {isLoading ? (
-            <div>Loading ...</div>
-         ) : (
-           <span>
-           {data.text}
-           </span>
-           )}
-        
-          </div>
-        </div>
-      </main>
-    </div>
+    <>
+      <SeoHead title='Welcome to SmartGuy' />
+      <Layout>
+        <Hero />
+        <Feature />
+        <Pricing />
+      </Layout>
+    </>
   );
 }
