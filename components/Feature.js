16e02608478246dbdb7c8f2966245f894416bfb7
TrustedSeven <117359800+TrustedSeven@users.noreply.github.com>
2022-11-10 13:10:57 -0600

    first updated

--- components/Feature.js
+++ components/Feature.js
@@ -37,7 +37,7 @@ const Feature = () => {
 
         <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
           <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600 mt-3">
-            We Provide Many Features You Can Use
+            We Provide Many Features You Can Use.
           </h3>
           <p className="my-2 text-black-500 mt-3">
             You can explore the features that we provide with amazing accurateness and have their
